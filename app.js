a67aedce82dfdc0600a40aa003935d3b3a94755c
Andre Goncalves <andre335@gmail.com>
2018-09-11 21:19:08 -0300

    adding morgan

--- app.js
+++ app.js
@@ -6,11 +6,27 @@ var live = require('./live/routes/live')
 var user = require('./user/routes/user')
 var streammer = require('./user/routes/roles/streammer')
 var viewer = require('./user/routes/roles/viewer')
+var morgan = require('morgan')
+var path = require('path')
+var rfs = require('rotating-file-stream')
+var fs = require('fs')
+
 const bodyParser = require('body-parser');
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+var logDirectory = path.join(__dirname, 'log')
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+var accessLogStream = rfs('access.log', {
+  interval: '1d', 
+  path: logDirectory
+})
+
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.use('/bet', bet)
 app.use('/calendar', calendar)
