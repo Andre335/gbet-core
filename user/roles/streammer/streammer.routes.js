eea3a3ba81ba49c2c5693a78d3c97b12821ddd24
Andre Goncalves <andre335@gmail.com>
2018-10-09 03:08:47 -0300

    User and Roles Docs

--- user/roles/streammer/streammer.routes.js
+++ user/roles/streammer/streammer.routes.js
@@ -2,14 +2,115 @@ const express = require('express')
 var Streammer = require('./streammer.controller')
 var router = express.Router()
 
+/**
+ * @swagger
+ * /streammer:
+ *   get:
+ *     tags:
+ *       - Streammer
+ *     description: Return all streammers
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: All streammers
+ *         schema:
+ *            $ref: '#/definitions/streammer '
+ *             
+ */
 router.get('/', Streammer.findAll)
 
+/**
+ * @swagger
+ * /streammer/{id}:
+ *   get:
+ *     tags:
+ *       - Streammer
+ *     description: Return one streammer with id: {id}
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Find one streammer
+ *         schema:
+ *            $ref: '#/definitions/streammer '
+ *             
+ */
 router.get('/:id', Streammer.findOne)
 
+/**
+ * @swagger
+ * /streammer/:
+ *   post:
+ *     tags:
+ *       - Streammer
+ *     description: Create one streammer
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Create one streammer
+ *         schema:
+ *            $ref: '#/definitions/streammer '
+ *             
+ */
 router.post('/', Streammer.create)
 
+/**
+ * @swagger
+ * /streammer/{id}:
+ *   delete:
+ *     tags:
+ *       - Streammer
+ *     description: Delete the streammer with id: {id}
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Delete one streammer
+ *         schema:
+ *            $ref: '#/definitions/streammer '
+ *             
+ */
 router.delete('/:id', Streammer.deleteById)
 
+/**
+ * @swagger
+ * /streammer/{id}:
+ *   put:
+ *     tags:
+ *       - Streammer
+ *     description: Update the streammer with id: {id}
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Update one streammer
+ *         schema:
+ *            $ref: '#/definitions/streammer '
+ *             
+ */
 router.put('/:id', Streammer.update)
 
 module.exports = router
+
+/**
+ * @swagger
+ * definition:
+ *   streammer:
+ *     properties:
+ *          user_id:
+ *              type: ObjectId
+ *          lives:
+ *              type: Array of ObjectId
+ */
--- user/user.routes.js
+++ user/user.routes.js
@@ -22,22 +22,184 @@ var router = express.Router()
  */
 router.get('/', User.findAll)
 
+/**
+ * @swagger
+ * /user/{id}:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: Return one user with id: {id}
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Find one user
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.get('/:id', User.findOne)
 
+/**
+ * @swagger
+ * /user/:
+ *   post:
+ *     tags:
+ *       - User
+ *     description: Create one User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Create one user
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.post('/', User.create)
 
+/**
+ * @swagger
+ * /user/{id}:
+ *   delete:
+ *     tags:
+ *       - User
+ *     description: Delete the user with id: {id}
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Delete one user
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.delete('/:id', User.deleteById)
 
+/**
+ * @swagger
+ * /user/{id}:
+ *   put:
+ *     tags:
+ *       - User
+ *     description: Update the user with id: {id}
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Update one user
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.put('/:id', User.update)
 
+/**
+ * @swagger
+ * /user/{id}/lives:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: Return all the user with id: {id} lives
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Return all the user lives
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.get('/:id/lives', User.findLivesByOwner)
 
+/**
+ * @swagger
+ * /user/{id}/bets:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: Return all the user with id: {id} bets
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Return all the user bets
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.get('/:id/bets', User.findBetsByOwner)
 
+/**
+ * @swagger
+ * /user/{id}/complaints/author:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: Return all the user with id: {id} complaints as author
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Return all the user complaints as author
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.get('/:id/complaints/author', User.findComplaintsByAuthor)
 
+/**
+ * @swagger
+ * /user/{id}/complaints/author:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: Return all the user with id: {id} complaints as accused
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Return all the user complaints as accused
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.get('/:id/complaints/accused', User.findComplaintsByAccused)
 
+/**
+ * @swagger
+ * /user/{id}/calendar:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: Return the user with id: {id} calendar
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         content: 
+ *            -application/json:
+ *         description: Return the user calendar
+ *         schema:
+ *            $ref: '#/definitions/user '
+ *             
+ */
 router.get('/:id/calendar', User.findCalendarByOwner)
 
 module.exports = router
